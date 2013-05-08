4ac6d9096ba3384aa0133cba817e6a1ed31cb40e
HTL WLA WEBU <alexander.hoor@student.htldornbirn.at>
2013-05-08 10:34:39 +0200

    design team code

---
+++ Code/DesignTeam/WebUntisApp/Design/DatePickerTest.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace WebUntisApp.Design
+{
+    public partial class Page2 : PhoneApplicationPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
